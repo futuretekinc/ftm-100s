8db2c5377f5f58e322220bd88a80141468b82a46
Inhyun Cho <xtra72@gmail.com>
2014-11-05 20:00:18 -0800

    U-boot 추가

---
+++ u-boot/include/config.h
@@ -0,0 +1,5 @@
+/* Automatically generated - do not edit */
+#define CONFIG_MK_FTM 1
+#define CONFIG_MK_NICO2 1
+#include <configs/ftm_nico2.h>
+#include <asm/config.h>
---
+++ u-boot/include/config.mk
@@ -0,0 +1,5 @@
+ARCH   = arm
+CPU    = arm_cortexa9
+BOARD  = ftm-nico2
+VENDOR = futuretek
+SOC    = goldengate
