e9432b64af370c4a0164ef9d0e29580d7b3889a9
Inhyun Cho <xtra72@gmail.com>
2014-11-05 21:31:31 -0800

    2.6.36 -> linux-2.6.36 으로 이름 변경

--- 2.6.36/arch/arm/mach-goldengate/cs_jt_init.c
+++
@@ -1,8 +0,0 @@
-/*
- * Cortina Systems 
- * 
- * This is auto generated. Please do not make modification to this file
- * For any changes, please update it to ,./bin/genCortinaJtInit.sh
- */
-struct cs_kernel_hw_accel_jt hw_jt = {
-};
